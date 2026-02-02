7ebaa867d93426a5373bbe4801874673c0050e06
AlexD39 <a-le30cm1@hotmail.com>
2026-02-01 22:34:25 -0600

    Freature/testJWT

--- backend/src/api/server.js
+++ backend/src/api/server.js
@@ -1,8 +1,5 @@
+require("dotenv").config();
 const { createApp } = require("./app");
 
 const PORT = process.env.PORT || 3000;
-const app = createApp();
-
-app.listen(PORT, () => {
-  console.log(`API listening on port ${PORT}`);
-});
+createApp().listen(PORT, () => console.log(`API listening on port ${PORT}`));
